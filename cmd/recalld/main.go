950abc086f7e3575093f4670c0414d5f902afcc2
Vic Cherubini <vcherubini@gmail.com>
2026-08-22 00:03:34 -0500

    wip

--- cmd/recalld/main.go
+++ cmd/recalld/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+    "flag"
 	"fmt"
 	"log"
 	"net/http"
+    "os"
+    "path/filepath"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,13 +14,28 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    homeDir, err := os.UserHomeDir()
+
+	if err != nil {
+		log.Fatalf("$HOME directory not found: %w", err)
+	}
+
+    dataDir := filepath.Join(homeDir, ".config", "recall")
+
+	dataDirPtr := flag.String("data-dir", dataDir, "recall data directory")
+	portPtr := flag.Int("port", 22550, "HTTP port to listen on")
+
+    flag.Parse()
+    fmt.Println("--data-dir=", *dataDirPtr)
+    fmt.Println("--port=", *portPtr)
+
 	// Register the route and handler
 	http.HandleFunc("/", helloHandler)
 
-	fmt.Println("Server starting on :8080...")
+	// fmt.Println("Server starting on :8080...")
 
 	// Start the server and block
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), nil)
 
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
